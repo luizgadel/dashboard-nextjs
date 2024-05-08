005da7f5262abd07f9cf777ecc433fcc004b6c3a
luizgadel <lpgds.eng16@uea.edu.br>
2024-05-07 20:25:44 -0400

    cap 7 done

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -5,6 +5,10 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
  
 export default async function Page() {
+    // const {revenue, latestInvoices} = await Promise.all([
+    //     fetchRevenue(),
+    //     fetchLatestInvoices()
+    // ])
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
     const {
