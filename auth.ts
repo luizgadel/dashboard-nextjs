ea03ad81bf65c3741f780e960798dae9f550c6f0
Luiz Gadelha <lpgds.eng16@uea.edu.br>
2024-05-13 00:48:27 -0400

    feat: login with backend request

--- auth.ts
+++ auth.ts
@@ -1,41 +1,43 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials"
-import { z } from "zod";
-import { sql } from '@vercel/postgres';
-import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
- 
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
-  }
-}
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [
         Credentials({
             async authorize(credentials) {
-                const parsedCredentials = z
-                .object({ email: z.string().email(), password: z.string().min(6) })
-                .safeParse(credentials)
+                const apiUrl = 'https://dashboard-nestjs.vercel.app';
+                const loginRoute = '/auth/login';
+                const profileRoute = '/auth/profile'
+                const loginResponse = await fetch(apiUrl + loginRoute, {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email: credentials.email, password: credentials.password }),
+                });
+                if (loginResponse.ok) {
+                    const responseData: { access_token: string } = await loginResponse.json()
+                    const jwt = responseData.access_token;
 
-                if (parsedCredentials.success) {
-                    const { email, password } = parsedCredentials.data;
-                    const user = await getUser(email);
-                    if (!user) return null;
-                    const passwordsMatch = await bcrypt.compare(password, user.password)
-
-                    if (passwordsMatch) return user;
+                    const profileResponse = await fetch(apiUrl + profileRoute, {
+                      method: 'GET',
+                      headers: {
+                        'Authorization': `Bearer ${jwt}`
+                      }
+                    });
+                    if (profileResponse.ok) {
+                      const user = await profileResponse.json();
+                      return user;
+                    } else {
+                      console.error('Failed to fetch user:', profileResponse.statusText);
+                      return null;
+                    }
+                } else {
+                  console.error('Failed to login:', loginResponse.statusText);
+                  return null;
                 }
-        
-                console.log('Invalid credentials');
-                return null;
             },
         }),
     ],
